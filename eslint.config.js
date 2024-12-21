c2a30b0a11f1a65f7f53f8b7330d07372f151452
alexandre.lecam <alex.lecam35@gmail.com>
2024-12-21 20:50:10 +0100

    eslint V2

--- eslint.config.js
+++ eslint.config.js
@@ -1,25 +1,33 @@
 import vuePlugin from "eslint-plugin-vue";
 import vueParser from "vue-eslint-parser";
-
-const { configs: vueConfigs } = vuePlugin;
+import tsParser from "@typescript-eslint/parser";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
 
 export default [
     {
         ignores: ["node_modules/**", "dist/**"], // Fichiers/dossiers à ignorer
     },
     {
-        files: ["**/*.vue"],
+        files: ["**/*.vue", "**/*.ts", "**/*.js"],
         languageOptions: {
-            parser: vueParser, // Utiliser vue-eslint-parser pour analyser les fichiers .vue
-            ecmaVersion: 2020,
-            sourceType: "module",
+            parser: vueParser, // Utilise vue-eslint-parser pour analyser les fichiers .vue
+            parserOptions: {
+                parser: tsParser, // Utilise TypeScript pour les blocs <script lang="ts">
+                ecmaVersion: 2020,
+                sourceType: "module",
+                ecmaFeatures: { jsx: true },
+            },
         },
         plugins: {
             vue: vuePlugin,
+            "@typescript-eslint": tsPlugin,
         },
         rules: {
-            ...vueConfigs["vue3-recommended"].rules,
-            "vue/multi-word-component-names": "off", // Désactiver la contrainte sur les noms multi-mots
+            ...vuePlugin.configs["vue3-recommended"].rules, // Règles recommandées pour Vue 3
+            ...tsPlugin.configs["recommended"].rules, // Règles recommandées pour TypeScript
+            "vue/script-setup-uses-vars": "error", // Évite les erreurs de variables non utilisées dans <script setup>
+            "@typescript-eslint/no-unused-vars": "error", // Avertit en cas de variables inutilisées
+            "vue/multi-word-component-names": "off", // Désactive la contrainte des noms multi-mots
         },
     },
 ];
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,11 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import daisyui from 'daisyui';
+
+export default {
   content: ["./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [require('daisyui')],
+  plugins: [daisyui],
   daisyui: {
     themes: ["light", "dark", "cupcake"], // Ajouter ou personnaliser des thèmes ici
   },
