85656d65f5fc230c326f702ba88dbde386bdef76
alexandre.lecam <alex.lecam35@gmail.com>
2024-12-21 21:45:13 +0100

    vue.config

--- vue.config.cjs
+++ vue.config.cjs
@@ -1,5 +1,16 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
-  transpileDependencies: true,
-})
+  css: {
+    loaderOptions: {
+      postcss: {
+        postcssOptions: {
+          plugins: {
+            tailwindcss: {},
+            autoprefixer: {},
+          },
+        },
+      },
+    },
+  },
+});
